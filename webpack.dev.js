3566798c9035d38318e9ef21921e4c6ba2426573
Xingchen Hong <hello@xc-h.com>
2018-01-08 17:09:05 -0600

    Allow the output directory to be changed with environment variable `DISTDIR`.

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,7 +17,7 @@ module.exports = {
     'web-gis-components': './src/web-gis-components.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, process.env.DISTDIR || 'dist'),
     filename: '[name].js',
   },
   resolve: {

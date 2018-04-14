d2aa44899d468960ed57778541727ff0861181b0
Xingchen Hong <hello@xc-h.com>
2018-04-14 03:39:32 -0700

    Add builds and move demo pages to docs directory for Github Page.

--- demo-server.js
+++ demo-server.js
@@ -3,6 +3,6 @@ var app = express();
 port = process.argv[2] || 4000;
 
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/docs/demo'));
 app.listen(port); //the port you want to use
 console.log("Express server running on port: "+port);
